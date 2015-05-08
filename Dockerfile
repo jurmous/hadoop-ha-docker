a0cdb58893f1fc6a303ee11551367e7bc9759eb3
Jurriaan Mous <jurmous@jurmo.us>
2015-05-08 12:37:47 +0200

    Add auto-failover, Auto stopping container on service termination, easy namenode formatting and bootstrapping, jq for command line json parsing (for etcd based fence scripts),

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,12 @@
 FROM java:7
 MAINTAINER jurmous
 
-# Define working directory.
-WORKDIR /data
+RUN curl -s http://www.eu.apache.org/dist/hadoop/common/hadoop-2.5.2/hadoop-2.5.2.tar.gz | tar -xz -C /usr/local/
+RUN cd /usr/local && ln -s ./hadoop-2.5.2 hadoop
 
-RUN curl -s http://www.eu.apache.org/dist/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz | tar -xz -C /usr/local/
-RUN cd /usr/local && ln -s ./hadoop-2.6.0 hadoop
-
-RUN apt-get install bash
+RUN apt-get update \
+  && apt-get install bash \
+  && apt-get install jq
 
 ENV USER root
 ENV JAVA_HOME /usr/lib/jvm/java-1.7.0-openjdk-amd64
@@ -20,6 +19,8 @@ ENV HADOOP_MAPRED_HOME /usr/local/hadoop
 ENV HADOOP_YARN_HOME /usr/local/hadoop
 ENV HADOOP_CONF_DIR /usr/local/hadoop/etc/hadoop
 ENV YARN_CONF_DIR $HADOOP_PREFIX/etc/hadoop
+
+WORKDIR /usr/local/hadoop
 
 RUN sed -i '/^export JAVA_HOME/ s:.*:export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64\nexport HADOOP_PREFIX=/usr/local/hadoop\nexport HADOOP_HOME=/usr/local/hadoop\n:' $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 RUN sed -i '/^export HADOOP_CONF_DIR/ s:.*:export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop/:' $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
@@ -39,5 +40,5 @@ RUN chown root:root /etc/fence.sh && chmod a+x /etc/fence.sh
 
 CMD ["/etc/bootstrap.sh", "-d"]
 
-# NameNode                Secondary NameNode  DataNode                     JournalNode  NFS Gateway    HttpFS
-EXPOSE 8020 50070 50470   50090 50495         50010 1004 50075 1006 50020  8485 8480    2049 4242 111  14000 14001 
+# NameNode                Secondary NameNode  DataNode                     JournalNode  NFS Gateway    HttpFS         ZKFC
+EXPOSE 8020 50070 50470   50090 50495         50010 1004 50075 1006 50020  8485 8480    2049 4242 111  14000 14001    8019
--- bootstrap.sh
+++ bootstrap.sh
@@ -30,47 +30,33 @@ fi
 checkArg $@
 shift $shift
 
-server="datanode"
+server="none"
 if [[ -z $1 ]]; then
-  # continue on with server as datanode 
-  echo Will start a DataNode
+  # start a bash
+  bash=true
 else
   server=$1
   shift 1
 fi
 
-if [ -z $NNODE1_IP ] || [ -z $NNODE2_IP ] || [ -z $JN_IPS ]; then
-  echo NNODE1_IP, NNODE2_IP and JN_IPS needs to be set as environment addresses to be able to run.
+if [ -z $NNODE1_IP ] || [ -z $NNODE2_IP ]  || [ -z $ZK_IPS ] || [ -z $JN_IPS ]; then
+  echo NNODE1_IP, NNODE2_IP, JN_IPS and ZK_IPS needs to be set as environment addresses to be able to run.
   exit;
 fi
 
-if [[ $NNODE1_IP = "localhost" ]]; then
-  NNODE1_IP=$HOSTNAME
-fi
 
-if [[ $NNODE2_IP = "localhost" ]]; then
-  NNODE2_IP=$HOSTNAME
-fi
-
-arr=$(echo $JN_IPS | tr "," "\n")
-JNODES=""
-for x in $arr
-do
-    if [ "$JNODES" != "" ]; then
-      JNODES+=";"
-    fi
-    JNODES+="$x:8485"
-done
+JNODES=$(echo $JN_IPS | tr "," ";")
 
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/hdfs-site.xml.template \
 | sed "s/NNODE1_IP/$NNODE1_IP/" \
 | sed "s/NNODE2_IP/$NNODE2_IP/" \
+| sed "s/ZKNODES/$ZK_IPS/" \
 | sed "s/JNODES/$JNODES/" \
 > /usr/local/hadoop/etc/hadoop/hdfs-site.xml
 
-echo CLUSTER_NAME=$CLUSTER_NAME NNODE1_IP=$NNODE1_IP NNODE2_IP=$NNODE2_IP JNODES=$JNODES
-
 sed "s/CLUSTER_NAME/$CLUSTER_NAME/" /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+
+echo SERVER=$server CLUSTER_NAME=$CLUSTER_NAME NNODE1_IP=$NNODE1_IP NNODE2_IP=$NNODE2_IP JNODES=$JNODES ZK_IPS=$ZK_IPS
 
 if [[ $server = "format" ]]; then
   read -p "Are you sure to format the hdfs volume? " -n 1 -r
@@ -83,12 +69,44 @@ if [[ $server = "format" ]]; then
   exit;
 fi
 
-echo $HADOOP_PREFIX/sbin/hadoop-daemon.sh start $server $@
+if [[ $server = "bootstrap" ]]; then
+  read -p "Are you sure to bootstrap the hdfs volume? " -n 1 -r
+  echo  
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    $HADOOP_PREFIX/bin/hadoop namenode -bootstrapStandby
+    exit;
+  fi
+  exit;
+fi
 
-$HADOOP_PREFIX/sbin/hadoop-daemon.sh start $server $@
+if [[ $server != "none" ]]; then
+  echo $HADOOP_PREFIX/sbin/hadoop-daemon.sh start $server $@
 
+  $HADOOP_PREFIX/sbin/hadoop-daemon.sh start $server $@
+  if [[ $server = "namenode" ]]; then
+    $HADOOP_PREFIX/sbin/hadoop-daemon.sh start zkfc
+  fi
+fi
+
+# Auto exit when the needed processes are not running
 if [[ $keeprunning = true ]]; then
-  while true; do sleep 1000; done
+  while true; do
+    # Only auto close when the daemon is not running. Else it could be a controlled stop
+    if [[ -z $(pgrep -f hadoop-daemon.sh) ]]; then
+      if [[ $server = "namenode" ]]; then
+        if [[ -z $(pgrep -f NameNode) ]]; then echo NameNode not running; $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop zkfc; exit 0; fi 
+        if [[ -z $(pgrep -f DFSZKFailoverController) ]]; then echo ZKFC not running; $HADOOP_PREFIX/sbin/hadoop-daemon.sh stop namenode; exit 0; fi 
+      elif [[ $server = "datanode" ]]; then
+        if [[ -z $(pgrep -f DataNode) ]]; then echo DataNode not running; exit 0; fi 
+      elif [[ $server = "journalnode" ]]; then
+        if [[ -z $(pgrep -f JournalNode) ]]; then echo JournalNode not running; exit 0; fi 
+      fi
+    else 
+      echo Hadoop daemon running
+    fi
+    sleep 3;
+  done
 fi
 
 if [[ $bash = true ]]; then
